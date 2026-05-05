60c62216d0deaf6b09f7db9b0e56b4d809dc065c
Mark Rode <rode.mark01@gmail.com>
2026-05-05 20:38:50 +0300

    Add client

--- client/src/main/kotlin/org/octopusden/octopus/reportingservice/client/ReportingServiceClient.kt
+++ client/src/main/kotlin/org/octopusden/octopus/reportingservice/client/ReportingServiceClient.kt
@@ -1,5 +1,13 @@
 package org.octopusden.octopus.reportingservice.client
 
+import feign.Headers
+import feign.RequestLine
+import org.octopusden.octopus.reportingservice.client.common.dto.buildconfig.BuildConfigurationReportRequestDto
+import org.octopusden.octopus.reportingservice.client.common.dto.buildconfig.BuildConfigurationReportResponseDto
+
 interface ReportingServiceClient {
 
+    @RequestLine("POST /rest/api/1/reports/build-configuration")
+    @Headers("Content-Type: application/json")
+    fun generateBuildConfigurationReport(request: BuildConfigurationReportRequestDto): BuildConfigurationReportResponseDto
 }
---
+++ client/src/main/kotlin/org/octopusden/octopus/reportingservice/client/ReportingServiceClientConfig.kt
@@ -0,0 +1,7 @@
+package org.octopusden.octopus.reportingservice.client
+
+data class ReportingServiceClientConfig(
+    val baseUrl: String,
+    val connectTimeoutMs: Long = 60_000,
+    val readTimeoutMs: Long = 100_000
+)
