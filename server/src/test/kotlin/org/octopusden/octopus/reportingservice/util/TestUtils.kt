d61b8cca9683ccd23f17360083a8baad5098f9cc
Mark Rode <rode.mark01@gmail.com>
2026-05-06 10:46:20 +0300

    Fix UT

--- server/src/test/kotlin/org/octopusden/octopus/reportingservice/util/TestUtils.kt
+++ server/src/test/kotlin/org/octopusden/octopus/reportingservice/util/TestUtils.kt
@@ -3,13 +3,14 @@ package org.octopusden.octopus.reportingservice.util
 import com.fasterxml.jackson.core.type.TypeReference
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
-import java.io.File
-import java.io.InputStream
 
 object TestUtils {
 
     val objectMapper: ObjectMapper = ObjectMapper().registerKotlinModule()
 
-    fun <T> loadObject(file: String, typeReference: TypeReference<T>): T =
-        objectMapper.readValue(File(file), typeReference)
+    fun <T> loadObject(resourcePath: String, typeReference: TypeReference<T>): T {
+        val stream = TestUtils::class.java.classLoader.getResourceAsStream(resourcePath)
+            ?: error("Resource '$resourcePath' not found in classpath")
+        return stream.use { objectMapper.readValue(it, typeReference) }
+    }
 }
